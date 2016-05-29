19183683f4a306ba404644b66808df3c540dbee9
UMSF-K13-1-Mushik <mushik1993@mail.ru>
2016-05-29 14:29:10 +0300

    commit

--- WindowsFormsApplication2/Form1.cs
+++ WindowsFormsApplication2/Form1.cs
@@ -19,7 +19,7 @@ namespace WindowsFormsApplication2
 
         private void button1_Click(object sender, EventArgs e)
         {
-            MessageBox.Show("Мушик Александр Сергеевич");
+            MessageBox.Show("Мушик Александр Сергеевич!");
         }
     }
 }
